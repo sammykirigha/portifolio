0ef40738d9d9c5b7125383519bc4c91104f85cc1
sammykirigha <dkirigha18@gmail.com>
2023-02-14 22:50:29 +0300

    added more changes

--- pages/About.tsx
+++ pages/About.tsx
@@ -39,6 +39,43 @@ const About = (props: Props) => {
         width="500"
         className="rounded-lg mb-10"
       />
+
+      <div className="flex flex-col bg-beige w-full mb-10 pb-10 items-center">
+        <h1 className="text-[1.3rem] text-center text-dark mt-4 font-[700]">
+          Hey Friends 👋, I'm Samuel. Nice to Meet You
+        </h1>
+        <div className="flex flex-col items-center justify-center max-w-[600px] mt-8">
+          <p className="text-center text-[1.1rem] text-dark">
+            I'm <strong className="text-light-red">Samuel Kirigha</strong>. I
+            graduated with a bachelor's degree in Computer Science from Karatina
+            University Of technology. I'm currently a Software Engineer working
+            with ReactJs, NodeJs, GraphQl, Postgres, MongoDB and TypeScript.
+          </p>
+          <p className="text-lg my-6 text-center text-[1.1rem]">
+            I'm also learning some DevOps tools on AWS and Azure.
+          </p>
+          <p className="text-lg text-center text-[1.1rem]">
+            The main purpose of starting my own portfolio is to showcase my
+            work, document what am learning through blogs and also to document
+            the books am reading by writing book notes. I hope you get to check
+            them out!
+          </p>
+        </div>
+      </div>
+
+      <div className="h-auto bg-white px-5 lg:px-20 2xl:px-60 pt-10 flex items-center justify-center">
+        <div className=" bg-[#efe0ca] mb-8 flex flex-col items-center justify-center h-72 w-[750px] rounded-lg">
+          <h1 className="text-[#001233] text-[2.5rem] font-[700] mt-8">
+            We can Work Together
+          </h1>
+          <p className="text-[1.5rem] text-[#001233] font-[500] mt-10">
+            Want to get your project done? Ready to deliver your expectation
+          </p>
+          <button className="mt-10 bg-[#efe0ca] border border-[#001233] text-[#001233] font-[600] hover:bg-[#001233] hover:text-[#efe0ca] py-2 px-5 rounded-full hover:tracking-widest text-lg hover:bg-opacity-90 transition-all duration-150 ease-linear  ">
+            Get In Touch
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
